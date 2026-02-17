0010148128382b801168c662d7de5d25133b97e7
Daniel Hilst Selli <392820+dhilst@users.noreply.github.com>
2026-02-17 12:20:40 -0300

    Add focus manager

---
+++ FocusManager.pm
@@ -0,0 +1,170 @@
+package FocusManager;
+
+use v5.36;
+use utf8;
+use Carp;
+use Scalar::Util qw(refaddr);
+
+use lib ".";
+use Utils qw(getters);
+use Event;
+
+getters qw(
+    widgets
+    index
+    key
+);
+
+sub new($widgets, %opts) {
+    confess "missing widgets" unless defined $widgets;
+    confess "widgets must be arrayref" unless ref($widgets) eq 'ARRAY';
+    my $key = $opts{-key} // "\t";
+    my $index = defined $opts{-index} ? int($opts{-index}) : 0;
+    if ($widgets->@* && ($index < 0 || $index > $widgets->$#*)) {
+        confess "index out of range";
+    }
+
+    my $self = bless {
+        widgets => $widgets,
+        index => $widgets->@* ? $index : -1,
+        key => $key,
+    }, __PACKAGE__;
+    $self->{widgets}->[$self->{index}]->focus if $self->{index} >= 0;
+    $self;
+}
+
+sub current($self) {
+    return undef if $self->{index} < 0;
+    $self->{widgets}->[$self->{index}];
+}
+
+sub cycle($self, $delta = 1) {
+    my $count = $self->{widgets}->@*;
+    return undef if $count == 0;
+    if ($self->{index} < 0) {
+        $self->{index} = 0;
+        my $cur = $self->{widgets}->[$self->{index}];
+        $cur->focus if $cur;
+        return $cur;
+    }
+    my $old = $self->{widgets}->[$self->{index}];
+    $self->{index} = ($self->{index} + $delta) % $count;
+    my $new = $self->{widgets}->[$self->{index}];
+    $old->blur if $old;
+    $new->focus if $new;
+    $new;
+}
+
+sub update($self, @events) {
+    my $changed = 0;
+    for my $event (@events) {
+        next unless $event->type eq Event::Type::KEY_PRESS;
+        my $char = $event->payload->char;
+        if ($char eq $self->{key}) {
+            $changed = 1 if $self->cycle(1);
+        }
+    }
+    $changed;
+}
+
+sub add_widget($self, $widget) {
+    confess "missing widget" unless defined $widget;
+    push $self->{widgets}->@*, $widget;
+    if ($self->{index} < 0) {
+        $self->{index} = 0;
+        $widget->focus;
+    }
+}
+
+sub remove_widget($self, $widget) {
+    my $widgets = $self->{widgets};
+    return if $widgets->@* == 0;
+    my $idx = -1;
+    for my $i (0 .. $widgets->$#*) {
+        if (ref($widgets->[$i]) && ref($widget)) {
+            if (refaddr($widgets->[$i]) == refaddr($widget)) {
+                $idx = $i;
+                last;
+            }
+            next;
+        }
+        if ($widgets->[$i] eq $widget) {
+            $idx = $i;
+            last;
+        }
+    }
+    return if $idx < 0;
+
+    my $old = $widgets->[$idx];
+    my $was_current = $idx == $self->{index};
+    $old->blur if $was_current;
+
+    splice $widgets->@*, $idx, 1;
+    if ($widgets->@* == 0) {
+        $self->{index} = -1;
+        return;
+    }
+
+    if ($was_current) {
+        my $new_idx = $idx % $widgets->@*;
+        $self->{index} = $new_idx;
+        $widgets->[$new_idx]->focus;
+    } elsif ($idx < $self->{index}) {
+        $self->{index}--;
+    }
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+FocusManager
+
+=head1 SYNOPSIS
+
+    use FocusManager;
+    my $fm = FocusManager::new(\@widgets);
+    $fm->update(@events);
+
+=head1 DESCRIPTION
+
+FocusManager cycles focus across a list of widgets. Each widget must
+implement C<focus> and C<blur>. By default the focus key is TAB.
+
+=head1 METHODS
+
+=over 4
+
+=item new($widgets, %opts)
+
+Creates a focus manager for the given widget arrayref. Options:
+
+- C<-key> key used for cycling focus (default: TAB)
+- C<-index> initial focused index (default: 0)
+An empty widget list is allowed; focus will be unset.
+
+=item update(@events)
+
+Processes events and advances focus when the cycle key is pressed.
+
+=item add_widget($widget)
+
+Adds a widget to the manager. If the list was empty, the new widget
+becomes focused.
+
+=item remove_widget($widget)
+
+Removes a widget. If it was focused, focus moves to the next widget.
+If the last widget is removed, focus becomes empty.
+
+=item cycle($delta)
+
+Moves focus by C<$delta> positions.
+
+=item current
+
+Returns the currently focused widget.
+
+=back
---
+++ t/FocusManager_test.t
@@ -0,0 +1,75 @@
+use v5.36;
+use Test::More;
+
+use lib ".";
+use FocusManager;
+use Event;
+
+{
+    package WidgetStub;
+    sub new($name) { bless { name => $name, focus => 0, blur => 0 }, __PACKAGE__ }
+    sub focus($self) { $self->{focus}++ }
+    sub blur($self) { $self->{blur}++ }
+    sub focus_count($self) { $self->{focus} }
+    sub blur_count($self) { $self->{blur} }
+}
+
+sub key($ch) { Event::key_press($ch) }
+
+subtest 'initial focus' => sub {
+    my $w1 = WidgetStub::new('a');
+    my $w2 = WidgetStub::new('b');
+    my $fm = FocusManager::new([$w1, $w2]);
+    is($w1->focus_count, 1, 'first widget focused');
+    is($w2->focus_count, 0, 'second not focused');
+};
+
+subtest 'cycles on tab' => sub {
+    my $w1 = WidgetStub::new('a');
+    my $w2 = WidgetStub::new('b');
+    my $fm = FocusManager::new([$w1, $w2]);
+    my $changed = $fm->update(key("\t"));
+    ok($changed, 'update reports change');
+    is($w1->blur_count, 1, 'first widget blurred');
+    is($w2->focus_count, 1, 'second widget focused');
+};
+
+subtest 'custom keybind' => sub {
+    my $w1 = WidgetStub::new('a');
+    my $w2 = WidgetStub::new('b');
+    my $fm = FocusManager::new([$w1, $w2], -key => 'n');
+    $fm->update(key('n'));
+    is($w1->blur_count, 1, 'blurred on custom key');
+    is($w2->focus_count, 1, 'focused on custom key');
+};
+
+subtest 'cycle wraps' => sub {
+    my $w1 = WidgetStub::new('a');
+    my $w2 = WidgetStub::new('b');
+    my $fm = FocusManager::new([$w1, $w2], -index => 1);
+    $fm->update(key("\t"));
+    is($w2->blur_count, 1, 'second blurred');
+    is($w1->focus_count, 1, 'wraps to first');
+};
+
+subtest 'add and remove widgets' => sub {
+    my $w1 = WidgetStub::new('a');
+    my $fm = FocusManager::new([]);
+    ok(!defined $fm->current, 'no current when empty');
+    ok(!$fm->update(key("\t")), 'no change when empty');
+
+    $fm->add_widget($w1);
+    is($w1->focus_count, 1, 'focus on first add');
+    is($fm->current, $w1, 'current set');
+
+    my $w2 = WidgetStub::new('b');
+    $fm->add_widget($w2);
+    $fm->remove_widget($w1);
+    is($w1->blur_count, 1, 'removed focused widget blurred');
+    is($fm->current, $w2, 'focus moved to next');
+
+    $fm->remove_widget($w2);
+    ok(!defined $fm->current, 'empty after removals');
+};
+
+done_testing;
